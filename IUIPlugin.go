e5fbd5ee51d7052432e88e279cd2bb71a9f1aebf
Gregor Pirolt <gregor.pirolt@me.com>
2020-09-15 23:19:15 +0200

    update UI to not force interface as first command; also write println and printf func signature to mirror fmt.println, printf

--- IUIPlugin.go
+++ IUIPlugin.go
@@ -2,11 +2,14 @@ package grgdplugincontracts
 
 // IUIPlugin ...
 type IUIPlugin interface {
-	ClearScreen(i interface{}) interface{}
-	PrintPercentOfScreen(i interface{}, str string, percent int) interface{}
-	PrintBanner(i interface{}) interface{}
-	PrintWorkloadOverview(i interface{})
-	PrintTable(i interface{}, heads []string, rows [][]string) interface{}
-	Println(i interface{}, str interface{}) interface{}
-	YesNoQuestion(i interface{}, question string) bool
+	ClearScreen(i ...interface{}) interface{}
+	PrintPercentOfScreen(startPercent int, endPercent int, i ...interface{}) interface{}
+	PrintBanner(i ...interface{}) interface{}
+	PrintTable(heads []string, rows [][]string, i ...interface{}) interface{}
+	Println(i ...interface{}) (int, error)
+	Printf(format string, i ...interface{}) (int, error)
+	YesNoQuestion(question string, i ...interface{}) bool
+	YesNoQuestionf(questionf string, i ...interface{}) bool
+	Question(question string, i ...interface{}) error
+	Questionf(questionf string, i ...interface{}) error
 }
